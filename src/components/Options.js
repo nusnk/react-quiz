4a3fb347343833278208afd27aba9e2c9e23fe54
Nurbolat Sansyzbay <nurbolatsnk@gmail.com>
2025-03-10 17:08:08 +0500

    Handled answer clicks with displaying incorrect answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,7 +1,20 @@
-export default function Options({ question }) {
+export default function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map( option => <button key={option} className="btn btn-option">{option}</button>)}
+      {question.options.map(
+        (option, index) => (
+          <button
+            key={option}
+            className={`btn btn-option
+              ${index === answer ? "answer" : "" }
+              ${hasAnswered ? index === question.correctOption ? "correct" : "wrong" : ""}
+            `}
+            disabled={hasAnswered}
+            onClick={() => dispatch({ type: "newAnswer", payload: index})}>
+              {option}
+          </button>
+        ))}
     </div>
   )
 }
